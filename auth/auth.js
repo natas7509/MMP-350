2f24afc195c8c732f93c59dc287b2a5c81bc8787
jon darling <jondarling@jons-mbp.bmcc.cuny.edu>
2019-09-18 15:04:26 -0400

    new account confirmation

--- auth/auth.js
+++ auth/auth.js
@@ -6,8 +6,9 @@ const message = document.getElementById("login-message");
 
 
 loginButton.onclick = function (e) {
-	//	message.textContent = emailInput.value + " has been submitted";
-	//	console.log(emailInput);
+message.textContent = emailInput.value + " has been submitted";
+console.log(emailInput);
+
 
 	firebase.auth().signInWithEmailAndPassword(emailInput.value, passwordInput.value);
 };
--- auth/create.js
+++ auth/create.js
@@ -1,13 +1,13 @@
-const loginButton = document.getElementById("login-button");
+const createUserButton = document.getElementById("create-button");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const message = document.getElementById("login-message");
 
-loginButton.onclick = function(e) {
-  //	message.textContent = emailInput.value + " has been submitted";
+createUserButton.onclick = function(e) {
+  message.textContent = emailInput.value + " congratulations";
   console.log(emailInput);
 
   firebase
     .auth()
-    .signInWithEmailAndPassword(emailInput.value, passwordInput.value);
+    .createUserWithEmailAndPassword(emailInput.value, passwordInput.value);
 };
