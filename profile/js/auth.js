8d2b5ea5a3193799bd8be1649910ef302fe8f684
jon darling <jondarling@jons-MacBook-Pro.local>
2019-10-10 01:31:07 -0400

    updated "Profile" to firebase

--- profile/js/auth.js
+++ profile/js/auth.js
@@ -3,39 +3,30 @@ const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const message = document.getElementById("login-message");
 
-
-//loginButton.onclick = function (e) {
-//	const promise = firebase
-//		.auth()
-//		.signInWithEmailAndPassword(emailInput.value, passwordInput.value);
-//	promise.catch(function (error) {
-//		message.textContent = error.message + " Perhaps it's time to get rid of hotmail ?";
-//	});
-//};
-
-
-loginButton.onclick = function (event) {
-	const promise = firebase.auth().signInWithEmailAndPassword(emailInput.value, passwordInput.value);
-	promise.catch(function (error) {
-		message.textContent = error.message + " Perhaps it's time to get rid of hotmail ?";
-	});
+loginButton.onclick = function (e) {
+  const promise = firebase
+    .auth()
+    .signInWithEmailAndPassword(emailInput.value, passwordInput.value);
+  promise.catch(function (error) {
+    message.textContent = error.message + " Perhaps it's time to get rid of hotmail ?";
+  });
 };
 
 /* auth state */
 const displayName = document.getElementById("user-name");
 
 firebase.auth().onAuthStateChanged(function (user) {
-	if (user) {
-		document.body.classList.add("auth");
-		displayName.textContent = "Welcome, " + user.displayName;
-	} else {
-		document.body.classList.remove("auth");
-		displayName.textContent = "";
-	}
+  if (user) {
+    document.body.classList.add("auth");
+    displayName.textContent = "Welcome, " + user.displayName;
+  } else {
+    document.body.classList.remove("auth");
+    displayName.textContent = "";
+  }
 });
 
 /* log out */
 const logoutButton = document.getElementById("logout-button");
 logoutButton.onclick = function () {
-	firebase.auth().signOut();
+  firebase.auth().signOut();
 };
--- profile/js/create.js
+++ profile/js/create.js
@@ -1,43 +1,11 @@
+// const createUserButton = document.getElementById("create-button");
+const userInput = document.getElementById("username");
 const createButton = document.getElementById("create-button");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
-const message = document.getElementById("login-message");
+// const message = document.getElementById("login-message");
+const errorMessage = document.getElementById("error-message");
 
-//createUserButton.onclick = function () {
-//	message.textContent = emailInput.value + " has been created!";
-//	console.log(emailInput);
-//
-//	firebase
-//		.auth()
-//		.createUserWithEmailAndPassword(emailInput.value, passwordInput.value);
-//};
-
-
-/*Adding to the database*/
-//createUserButton.onclick = function () {
-//	const promise = firebase.auth().createUserWithEmailAndPassword(emailInput.value, passwordInput.value);
-//	promise.catch(function (error) {
-//		errorMessage.textContent = error.message;
-//	});
-//
-//	promise.then(function (credential) {
-//		createUser(credential.user.uid);
-//	});
-//};
-//
-//function createUser(uid) {
-//	const db = firebase.database();
-//	const ref = db.ref("users").child(uid);
-//	const promise = ref.update({
-//		displayName: userInput.value
-//	});
-//
-//	promise.then(function () {
-//		location.href = "index.html";
-//	});
-//}
-
-/*Adding to the database*/
 createButton.onclick = function () {
 	const promise = firebase.auth().createUserWithEmailAndPassword(emailInput.value, passwordInput.value);
 	promise.catch(function (error) {
