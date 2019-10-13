8977102cc0cfe6586bd06ddc33a300f914424eec
jon darling <jondarling@jons-MacBook-Pro.local>
2019-10-13 02:16:11 -0400

    config - index for js

--- theResurectionSite/js/main.js
+++ theResurectionSite/js/main.js
@@ -6,7 +6,7 @@ function initMap() {
     lng: -73.92719
   };
   // Centered map on location
-  const map = new google.maps.Map(document.querySelector('.map'), {
+  const map = new google.maps.Map(document.querySelector(".map"), {
     zoom: 14,
     center: loc
   });
@@ -18,23 +18,22 @@ function initMap() {
 }
 
 // Sticky menu background
-window.addEventListener('scroll', function () {
+window.addEventListener("scroll", function () {
   if (window.scrollY > 150) {
-    document.querySelector('#navbar').style.opacity = 0.9;
+    document.querySelector("#navbar").style.opacity = 0.9;
   } else {
-    document.querySelector('#navbar').style.opacity = 1;
+    document.querySelector("#navbar").style.opacity = 1;
   }
 });
 
-
 // Smooth Scrolling
-$('#navbar a, .btn').on('click', function (event) {
-  if (this.hash !== '') {
+$("#navbar a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
     event.preventDefault();
 
     const hash = this.hash;
 
-    $('html, body').animate({
+    $("html, body").animate({
         scrollTop: $(hash).offset().top - 100
       },
       800
