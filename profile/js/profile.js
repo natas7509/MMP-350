35ec341f2cbffc8e9fe0293e5d4736d78c3592c5
jon darling <jondarling@jons-MacBook-Pro.local>
2019-10-18 09:32:42 -0400

    added "gender, website" to Profile

--- profile/js/profile.js
+++ profile/js/profile.js
@@ -4,6 +4,12 @@ const profileName = document.getElementById('profile-name');
 const bioInput = document.getElementById('bio');
 const updateButton = document.getElementById('update-profile');
 
+// ---------------------------------------------------------------
+
+const phoneInput = document.getElementById('phone');
+const genderInput = document.getElementById('gender');
+const websiteInput = document.getElementById('website');
+
 
 userRef.on('value', function (snapshot) {
 	const userInfo = snapshot.val();
@@ -12,11 +18,39 @@ userRef.on('value', function (snapshot) {
 	if (userInfo.bio) {
 		bioInput.value = userInfo.bio;
 	}
+	if (userInfo.phone) {
+		phoneInput.value = userInfo.phone;
+	}
+	if (userInfo.gender) {
+		genderInput.value = userInfo.gender;
+	}
+	if (userInfo.website) {
+		websiteInput.value = userInfo.website;
+	}
 });
 
 updateButton.onclick = function () {
 	userRef.update({
 		displayName: profileName.value,
-		bio: bioInput.value
+		bio: bioInput.value,
+		phone: phoneInput.value,
+		gender: genderInput.value,
+		website: websiteInput.value
 	});
 };
+
+// userRef.on('value', function (snapshot) {
+// 	const userInfo = snapshot.val();
+// 	profileName.value = userInfo.displayName;
+
+// 	if (userInfo.bio) {
+// 		bioInput.value = userInfo.bio;
+// 	}
+// });
+
+// updateButton.onclick = function () {
+// 	userRef.update({
+// 		displayName: profileName.value,
+// 		bio: bioInput.value
+// 	});
+// };
