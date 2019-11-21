3330b458aec6bdc28ade643966829d9a8620863f
Jon Darling <jon.darling@f1006-02-34136.bmcc.cuny.edu>
2019-11-20 21:29:36 -0500

    query link test

--- query/js/user.js
+++ query/js/user.js
@@ -5,8 +5,24 @@ const postRef = firebase.database().ref('posts').orderByChild('uid').equalTo(uid
 const userRef = firebase.database().ref('users').child(uid);
 
 userRef.on('value', function (snapshot) {
-    const userInfo = snapshot.val();
-    postRef.on('child_added', function (snapshot) {
-        createPost(snapshot.val(), userInfo);
-    });
+	const userInfo = snapshot.val();
+	postRef.on('child_added', function (snapshot) {
+		createPost(snapshot.val(), userInfo);
+	});
 });
+
+
+
+
+
+
+
+
+
+/*CHANGE THIS PAGE*/
+
+/*ALSO HIS AUTH.CSS RULES*/
+/*CHECK OUT USER.HTML NAV STUFF*/
+//ADD USERS.JS
+
+/*ADD NEW USERS.CSS PAGE TO THE CSS PAGE STYLES SECTION*/
