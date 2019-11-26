84c69adcd425bef3829e501cea141f19a6d51b4b
Jon Darling <jon.darling@f1006-02-34136.bmcc.cuny.edu>
2019-11-25 21:19:42 -0500

    fixing firebase errors

--- storage/js/posts.js
+++ storage/js/posts.js
@@ -2,75 +2,79 @@ const posts = document.getElementById("posts");
 const postRef = firebase.database().ref("posts");
 
 function loadPosts() {
-  postRef.on('child_added', function (snapshot) {
-    createPost(snapshot.val());
-  });
+	postRef.on('child_added', function (snapshot) {
+		createPost(snapshot.val());
+	});
 }
 
 function createElement(_class, text) {
-  const element = document.createElement("div");
-  element.classList.add(_class);
-  element.textContent = text;
-  return element;
+	const element = document.createElement("div");
+	element.classList.add(_class);
+	element.textContent = text;
+	return element;
 }
 
+
+/*THIS WAS THE FIREBASE ERROR FIX*/
 function createPost(data) {
-  const post = createElement("post");
-  const text = createElement("text", data.text);
-  const author = createElement("author", "by " + users[data.uid].displayName);
+	const post = createElement("post");
+	const text = createElement("text", data.text);
+	var userData = users[data.uid] ? users[data.uid] : {
+		displayName: "anonymous"
+	};
+	const author = createElement("author", "by " + userData.displayName);
 
-  var d = new Date(data.date);
-  const date = createElement(
-    "date",
-    d.getMonth() + 1 + "." + d.getDate() + "." + d.getFullYear()
-  );
+	var d = new Date(data.date);
+	const date = createElement(
+		"date",
+		d.getMonth() + 1 + "." + d.getDate() + "." + d.getFullYear()
+	);
 
-  //	posts.appendChild(post);
-  posts.insertBefore(post, posts.firstElementChild);
+	//	posts.appendChild(post);
+	posts.insertBefore(post, posts.firstElementChild);
 
-  /* adding user profile image */
-  const img = new Image();
-  if (users[data.uid].imageURL) {
-    img.src = users[data.uid].imageURL;
-  } else {
-    img.src = "images/logo.png";
-  }
-  img.classList.add("profile-image");
+	/* adding user profile image */
+	const img = new Image();
+	if (userData.imageURL) {
+		img.src = userData.imageURL;
+	} else {
+		img.src = "images/logo.png";
+	}
+	img.classList.add("profile-image");
 
-  post.appendChild(img);
-  post.appendChild(text);
-  post.appendChild(author);
-  post.appendChild(date);
+	post.appendChild(img);
+	post.appendChild(text);
+	post.appendChild(author);
+	post.appendChild(date);
 
 
-
-  //  adding ATTACHMENT image 
-  // const attachImg = new Image();
-  // if (users[data.uid].imageURL) {
-  //   attachImg.src = users[data.uid].imageURL;
-  // } else {
-  //   attachImg.src.style.display = "none";
-  // }
-  // attachImg.classList.add("attach-image");
+	//  adding ATTACHMENT image 
+	const attachImg = new Image();
+	if (data.imageURL) {
+		attachImg.src = data[data.uid].imageURL;
+	} else {
+		attachImg.src.style.display = "none";
+	}
+	attachImg.classList.add("attach-image");
 }
 
 // get users
 let userCount = 0;
 const users = {};
 firebase
-  .database()
-  .ref("users")
-  .on("child_added", function (snapshot) {
-    // users[snapshot.key] = snapshot.val().displayName;
-    users[snapshot.key] = snapshot.val();
-    userCount += 1;
-  });
+	.database()
+	.ref("users")
+	.on("child_added", function (snapshot) {
+		// users[snapshot.key] = snapshot.val().displayName;
+		users[snapshot.key] = snapshot.val();
+		userCount += 1;
+	});
 
 firebase
-  .database()
-  .ref("users")
-  .once("value", function (snapshot) {
-    if (userCount === snapshot.numChildren()) {
-      loadPosts();
-    }
-  });
+	.database()
+	.ref("users")
+	.once("value", function (snapshot) {
+		if (userCount === snapshot.numChildren()) {
+			loadPosts();
+		}
+	});
--- storage/js/publish.js
+++ storage/js/publish.js
@@ -1,5 +1,6 @@
 const postText = document.getElementById('post-text');
 const publishButton = document.getElementById('publish');
+//const updateButton = document.getElementById('attach-image');
 
 publishButton.addEventListener('click', publishPost);
 postText.addEventListener('keyup', function (event) {
@@ -18,5 +19,36 @@ function publishPost() {
 	postText.value = "";
 
 	// push post to database
-	ref.push(post);
+
+	const promise = ref.push(post);
+	promise.then(function (snapshot) {
+		addImage(snapshot.key);
+	});
 }
+
+function addImage(postId) {
+	if (file) {
+		// upload the file
+		const storage = firebase.storage();
+		const ref = storage.ref('posts').child(postId).child('attach-image');
+		const promise = ref.put(file);
+
+		promise.then(function (image) {
+			return image.ref.getDownloadURL();
+		}).then(function (url) {
+			firebase.database().ref('posts').child(postId).update({
+				imageURL: url
+			});
+		});
+	}
+}
+
+
+//WORK ON BELOW TO GET YOUR IMAGETOPOST TO WORK
+let file;
+
+const imageButton = document.getElementById('submit-image');
+imageButton.addEventListener('click', function () {
+	// get the file
+	file = document.getElementById('image-file').files[0];
+});
